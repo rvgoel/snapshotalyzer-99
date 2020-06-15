4dbe8fed4de24cf2be0534258402a5f1c7b06a23
Ravi Goel <rvdelhi85@gmail.com>
2020-06-15 22:46:22 +0530

    Dont start a new snapshot if one is already in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots=list(volume.snapshot.all())
+    return snapshots and snapshots[0].state=='pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -84,8 +88,12 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
-            print("Creating snapshot for {0}".format(v.id))
-            v.create_snapshot(Description="Created by snapshotalyzer99")
+            if has_pending_snapshot(v):
+                print("Skipping {0}, snapshot already in progress".format(v.id))
+                continue
+            else:
+                print("Creating snapshot for {0}".format(v.id))
+                v.create_snapshot(Description="Created by snapshotalyzer99")
 
         print("Starting {0}...".format(i.id))
         i.start()
