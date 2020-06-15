9c34d335d0e576ec11cd04721831b07680ec2291
Ravi Goel <rvdelhi85@gmail.com>
2020-06-15 22:18:44 +0530

    Create snapshot functionality added

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -77,10 +77,19 @@ def create_snapshots(project):
     instances=filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
             print("Creating snapshot for {0}".format(v.id))
             v.create_snapshot(Description="Created by snapshotalyzer99")
 
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!!")
     return
 
 @instances.command('list')
